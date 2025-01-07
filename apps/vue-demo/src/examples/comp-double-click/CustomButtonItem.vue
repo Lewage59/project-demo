4dcd4962b1032165bf034e1234bc19c56a4a8bf3
Lewage59 <liwj.walker@foxmail.com>
2025-01-07 21:46:29 +0800

    feat: click comp

---
+++ apps/vue-demo/src/examples/comp-double-click/CustomButtonItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="custom-button-item" @click="handleCustomButtonItemClick">
+    custom button item
+  </div>
+</template>
+
+<script setup>
+const emits = defineEmits(["a-click"]);
+
+const handleCustomButtonItemClick = () => {
+  console.log("CustomButtonItem click handleClick");
+  emits("a-click");
+};
+</script>
+
+<style scoped>
+.custom-button-item {
+  margin: 20px;
+  background-color: red;
+}
+</style>
